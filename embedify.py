6324295e661125f93400bf6790488d76e4cd8d27
PRIZ ;] <prizmdev@outlook.com>
2020-08-27 15:02:33 -0500

    Add files via upload

---
+++ embedify.py
@@ -0,0 +1,121 @@
+### IMPORTANT NOTICE
+# This file is almost a 100% copy from my own bot. I've written this code a
+# long time ago and am too lazy to change it. It works great, so I won't
+# recommend changing it.
+
+import discord, datetime
+from discord import Embed
+import discord, re
+#from util.PrizErr import *
+global NA
+NA = Embed.Empty
+
+def embedify(
+    ##/// QUICK KWARGS
+    title = "PRIZM ;]", desc="",
+    thumb = "", foot = "",
+    img = "",  url = "", time = "",
+    color=0x00ffff, auth = "",
+    foot_ico = "", auth_url = "",
+    auth_ico = "", fields = [], typ="rich"
+    ) -> discord.Embed:
+    ################################################################
+    # >>> CREATES AN EMBED <<<
+    # Is designed to make what would've been many commands into one
+    #
+    # TITLE    [STR     ] - The Title
+    # DESC     [STR     ] - Description
+    # THUMB    [STR     ] - HTTP[S] Url for thumbnail
+    # FOOT     [STR     ] - The footer
+    # IMG      [STR     ] - HTTP[S] Url for image
+    # URL      [STR     ] - HTTP[S] Url for when the embed is tapped on
+    #
+    # TIME     [DATETIME] - Datetime instance for the timestamp
+    #                     - if time == 'now', then the current time will be used
+    #                     - ^ that is a timesaver on purpose
+    #
+    # COLOR    [HEX     ] - The color of the embed, in hex
+    # AUTH     [STR     ] - The author, places on top of the title
+    # FOOT_ICO [STR     ] - HTTP[S] Url for footer icon
+    # AUTH_URL [STR     ] - I'm not too sure
+    # AUTH_ICO [STR     ] - HTTP[S] Url for author icon
+    # FIELDS   [LIST    ] - List of fields [[name: str, desc: str, inline: bool], ...]
+    # TYP      [STR     ] - The embed type, "rich" by default
+    #
+    # *All URLs must be HTTP[S], otherwise the function will silently ignore it
+    # *Any params not specified will be an empty string or the Embed.Empty thing
+    ################################################################
+    ##/// Check for valid time
+    if time == 'now': time = datetime.datetime.utcnow()
+    elif not time: time = NA
+    elif type(time) != datetime.datetime: time = NA
+
+    ##/// Check for valid URLs
+    url_regex = r"https?\://"
+    if not re.search(url_regex, str(auth_ico)): auth_ico = ""
+    if not re.search(url_regex, str(url)): url = ""
+    if not re.search(url_regex, str(thumb)): thumb = ""
+    if not re.search(url_regex, str(img)): img = ""
+    if not re.search(url_regex, str(auth_ico)): auth_ico = ""
+
+    ##/// Create the embed
+    emb = discord.Embed(
+        title = str(title) or NA,
+        description = str(desc) or "",
+        color = color or 0x00ffff,
+        url = str(url) or NA,
+        timestamp = time or NA,
+        type = str(typ) or "rich"
+    )
+    emb.set_footer(text = str(foot) or NA, icon_url = str(foot_ico) or NA)
+    if thumb: emb.set_thumbnail(url = str(thumb))
+    if auth: emb.set_author(name = str(auth), icon_url = str(auth_ico)or NA, url = str(auth_url) or NA)
+    if img: emb.set_image(url = str(img))
+    return embfield(emb, fields)
+
+def embfield(emb, fld):
+    fields = fld
+    ################################################################
+    # >>> ADDS FIELD TO A GIVEN EMBED <<<
+    # Is designed to make what would've been many commands into one
+    #
+    # EMB    [Embed] - The embed instance
+    # FIELDS [LIST ] - List of fields [[name: str, value: str, inline: bool*], ...]
+    # *I actually support more than bools as it is converted into a string and check for manually
+    ################################################################
+    for field in fields:
+        if len(field) == 3:
+            emb.add_field(name = str(field[0]),
+                      value = str(field[1]),
+                      inline = str(field[2]) in ["True", "true", "y", "t", "ye", "yes", "1"])
+        elif len(field) == 2:
+            emb.add_field(name = str(field[0]),
+                      value = str(field[1]),
+                      inline = False)
+        elif len(field) == 1:
+            emb.add_field(name = ";]",
+                          value = str(field[0]),
+                          inline = False)
+        else:
+            pass#raise PrizmEmbedifyError(field)
+    return emb
+
+def emb_compat(
+    ##/// COMPATIBLE KWARGS [CHECK D.PY DOCS]
+    title = 'PRIZM ;]', description = '',
+    thumbnail = "", footer = "",
+    url = "", timestamp = "",
+    image = "", author = "",
+    author_icon = "", author_url = "",
+    footer_icon = "", fields = [],
+    type = "rich"
+    ) -> discord.Embed:
+    return embedify(title = title, desc = description, thumb = thumbnail,
+                    url = url, time = timestamp, img = image, auth = author,
+                    auth_ico = author_icon, auth_url = author_url, typ = type,
+                    foot = footer, foot_ico = footer_icon, fields = fields)
+
+##/// SHORTER CALL METHODS for ease of use
+def emb(*args, **kwargs): return embedify(*args, **kwargs)
+def emb_c(*args, **kwargs): return emb_compat(*args, **kwargs)
+def emb_f(*args, **kwargs): return embfield(*args, **kwargs)
---
+++ starboard.py
@@ -0,0 +1,193 @@
+import discord
+import asyncio
+from discord.ext import commands
+from discord.ext.commands import Bot
+import embedify
+import database as dbman
+
+bot = commands.Bot(
+    command_prefix = "!starbamiel"
+)
+
+# Add your list of emojis
+emojis = [
+    "✨",
+    "🌟",
+    "🎇",
+    "⭐",
+]
+
+# Set the emoji count. This will be the sum of all emojis,
+# so 2✨ and 3⭐ would be 5
+count = 5
+
+# Enter the bot token here
+TOKEN = "<TOKEN>"
+
+# Only used when the reaction emoji is completely missing
+class FalseReaction:
+    def __init__(self, emoji, message):
+        self.count = 0
+        self.me = False
+        self.emoji = emoji
+        self.custom_emoji = emoji.is_custom_emoji()
+        self.message = message
+
+# Get the reaction and the user from the payload
+async def from_rct_payload(payload):
+    chn = await bot.fetch_channel(payload.channel_id)
+    msg = await chn.fetch_message(payload.message_id)
+    for reaction in msg.reactions:
+        if str(reaction.emoji) == str(payload.emoji):
+            return reaction, chn.guild.get_member(payload.user_id)
+    reaction = FalseReaction(payload.emoji, msg)
+    return reaction, chn.guild.get_member(payload.user_id)
+
+# Updates the starboard
+async def plug_starboard(msg, emojis):
+    stars = msg.guild.get_channel(389571709470113793)
+    starred = []
+    for reaction in msg.reactions:
+        if str(reaction.emoji) in emojis:
+            starred.append(str(reaction.count) + "x" + str(reaction.emoji))
+
+    embed = embedify.embedify(
+        title = 'STARBOARD ;]',
+        desc = msg.content,
+        fields = [
+            ['AUTHOR', f'<@{msg.author.id}> `{str(msg.author)}`', True],
+            ['CHANNEL', f'<#{msg.channel.id}>', True],
+            [
+                'LINK',
+                f'[JUMP](https://discord.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id})',
+                True
+            ],
+            ['STARS', '\n'.join(starred), True]
+        ],
+        thumb = str(msg.author.avatar_url)
+    )
+    attachments = [att.url for att in msg.attachments]
+    if len(attachments):
+        embed.add_field(
+            name = 'ATTACHMENTS',
+            value = '\n'.join(attachments),
+            inline = False
+        )
+        try:
+            msg.attachments[0].height
+            embed.set_image(url = attachments[0])
+        except:
+            pass
+    stars_id = dbman.get('starboard', 'starboard_id', message_id = msg.id)
+    try:
+        if not stars_id:
+            raise TypeError("stars_id isn't available yet")
+        stars_msg = await stars.fetch_message(stars_id)
+        await stars_msg.edit(embed = embed)
+    except:
+        stars_msg = await stars.send(embed = embed)
+        dbman.insert('starboard', starboard_id = stars_msg.id, message_id = msg.id)
+
+
+# Reaction remove only...
+async def handle_reaction_remove(reaction, user):
+    global emojis, count
+    chn = reaction.message.channel
+    try:
+        if user.id == bot.user.id:
+            return
+        msg = reaction.message
+        if str(reaction.emoji) in emojis:
+            # Count all stars
+            cc = 0
+            for r in msg.reactions:
+                cc += r.count if str(r.emoji) in emojis else 0
+            # Check if above threshold
+            if count <= cc and count > 0:
+                await plug_starboard(msg, emojis)
+            # Check if below threshold
+            elif count > 0 and count > cc:
+                stars = msg.guild.get_channel(dbman.get('star', 'channel', id = msg.guild.id))
+                stars_id = dbman.get('starboard', 'starboard_id', message_id = msg.id)
+                if stars_id:
+                    stars_msg = await stars.fetch_message(stars_id)
+                    await stars_msg.delete()
+                    dbman.remove('starboard', starboard_id = stars_id, message_id = msg.id)
+    except IndexError:
+        pass
+    except Exception as ex:
+        await chn.send(f"`{ex}` line {ex.__traceback__.tb_lineno}")
+
+
+# Reaction add only
+async def handle_reaction_add(reaction, user):
+    global emojis, count
+    chn = reaction.message.channel
+    try:
+        if user.id == bot.user.id:
+            return
+        msg = reaction.message
+        mng = False
+        try:
+            # Check if it's a starboard message
+            if msg.embeds[0].title == "STARBOARD ;]":
+                await chn.send(
+                    f"<@{user.id}>```diff\n-] YOU CAN'T STAR THE STARBOARD```",
+                    delete_after = 10.0
+                )
+                await reaction.remove(user)
+                mng = True
+        except IndexError:
+            mng = False
+        if str(reaction.emoji) in emojis and not mng:
+            # Check if the user is starring their own message
+            if user.id == msg.author.id:
+                await chn.send(
+                    f"<@{user.id}>```diff\n-] YOU CAN'T STAR YOUR OWN MESSAGES```",
+                    delete_after = 10.0
+                )
+                await reaction.remove(user)
+            else:
+                # Count all stars
+                cc = 0
+                for r in msg.reactions:
+                    cc += r.count if str(r.emoji) in emojis else 0
+                if count <= cc and count > 0:
+                    await plug_starboard(msg, emojis)
+    except IndexError:
+        pass
+    except Exception as ex:
+        await chn.send(f"`{ex}` line {ex.__traceback__.tb_lineno}")
+
+@bot.listen()
+async def on_raw_reaction_add(payload):
+    reaction, user = await from_rct_payload(payload)
+    return await handle_reaction_add(reaction, user)
+
+@bot.listen()
+async def on_raw_reaction_remove(payload):
+    reaction, user = await from_rct_payload(payload)
+    return await handle_reaction_remove(reaction, user)
+
+@bot.listen()
+async def on_raw_message_delete(payload):
+    stars_id = dbman.get('starboard', 'starboard_id', message_id = payload.message_id)
+    if stars_id:
+        dbman.remove('starboard', starboard_id = stars_id, message_id = payload.message_id)
+        return
+    msg_id = dbman.get('starboard', 'message_id', starboard_id = payload.message_id)
+    if msg_id:
+        dbman.remove('starboard', starboard_id = payload.message_id, message_id = msg_id)
+        return
+
+@bot.listen()
+async def on_ready():
+    await bot.change_presence(
+        activity = discord.Activity(
+            type = 3,
+            name = f"the stars fly by 0.0"
+        ),
+        status = discord.Status.idle
+    )
+
+bot.run(TOKEN)
